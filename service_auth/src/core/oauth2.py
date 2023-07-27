10e3f507f5c06cfb5d82acfa3b97e3a365068aa8
Nuraiym Obozkanova <nuraika.obozkanova@gmail.com>
2023-07-27 12:42:44 +0600

    env separation

--- service_auth/src/core/oauth2.py
+++ service_auth/src/core/oauth2.py
@@ -3,20 +3,20 @@ from typing import List
 from async_fastapi_jwt_auth import AuthJWT
 from pydantic import BaseModel
 
-from .config import settings
+from .config import jwt_settings
 
 
 class Settings(BaseModel):
-    authjwt_algorithm: str = settings.JWT_ALGORITHM
-    authjwt_decode_algorithms: List[str] = [settings.JWT_ALGORITHM]
+    authjwt_algorithm: str = jwt_settings.JWT_ALGORITHM
+    authjwt_decode_algorithms: List[str] = [jwt_settings.JWT_ALGORITHM]
     authjwt_token_location: set = {'cookies', 'headers'}
     authjwt_access_cookie_key: str = 'access_token'
     authjwt_refresh_cookie_key: str = 'refresh_token'
     authjwt_cookie_csrf_protect: bool = True
     authjwt_public_key: str = base64.b64decode(
-        settings.JWT_PUBLIC_KEY).decode('utf-8')
+        jwt_settings.JWT_PUBLIC_KEY).decode('utf-8')
     authjwt_private_key: str = base64.b64decode(
-        settings.JWT_PRIVATE_KEY).decode('utf-8')
+        jwt_settings.JWT_PRIVATE_KEY).decode('utf-8')
 
 
 @AuthJWT.load_config
